74dbc4bd707e8ae88f4b3649a5a4fdd76b41532c
Nidhi Rathi <nidhirathi2312@gmail.com>
2022-04-15 19:19:28 +0530

    First Commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Styles
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="copyright">&copy; React CRUD App created by NIDHI RATHI 19BCE2004</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// Styles
+import "./style.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <a href="/" className="logo">
+          React CRUD Application - NIDHI RATHI 19BCE2004
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

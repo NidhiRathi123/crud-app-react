c4357b1bebfe7ec5546e16ddb7a489606218bfba
Nidhi Rathi <nidhirathi2312@gmail.com>
2022-04-15 19:20:34 +0530

    Updates

---
+++ precache-manifest.28b8d2b41fb05ea59ed8a334174937fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3b94ae6ab03ecf57e67ae9ffddc22a",
+    "url": "/crud-app-react/index.html"
+  },
+  {
+    "revision": "e95362a0b48b6b149764",
+    "url": "/crud-app-react/static/css/main.5ce56e49.chunk.css"
+  },
+  {
+    "revision": "9172b019bc4859d1afcf",
+    "url": "/crud-app-react/static/js/2.6b593ac7.chunk.js"
+  },
+  {
+    "revision": "d7b6ce2de861a9d04626a0650ef3727b",
+    "url": "/crud-app-react/static/js/2.6b593ac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95362a0b48b6b149764",
+    "url": "/crud-app-react/static/js/main.d18990aa.chunk.js"
+  },
+  {
+    "revision": "8ab65af098661ad7ed91",
+    "url": "/crud-app-react/static/js/runtime-main.4ef3e933.js"
+  },
+  {
+    "revision": "6a51178c4c213bbde5487bfb5f652177",
+    "url": "/crud-app-react/static/media/placeholder-user.6a51178c.jpg"
+  }
+]);
